41d9573292da5fb05f6616db67aff6b4abba2e7f
Tarik <tooto75@protonmail.com>
2025-12-01 19:49:49 +0100

    build err

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,8 +5,10 @@ import "./i18n/config";
 
 // Initialize seed data on first load (development only)
 if (import.meta.env.DEV) {
-  const { initializeSeedData } = await import("./data/seedData");
-  initializeSeedData();
+  (async () => {
+    const { initializeSeedData } = await import("./data/seedData");
+    initializeSeedData();
+  })();
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
